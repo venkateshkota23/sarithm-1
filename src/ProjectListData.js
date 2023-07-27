8e61a0289e8d58457cd6d92464b0048dd72d7618
venkateshkota23 <68192802+venkateshkota23@users.noreply.github.com>
2023-07-27 17:33:49 +0530

    Add files via upload

---
+++ src/ProjectListData.js
@@ -0,0 +1,98 @@
+export const projects = [
+    {
+      projectName: "Project 1",
+      projectDescription:
+        "As an early adopter of containers and kubernetes on AWS, the company's site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization.",
+      projectFullDescription:
+      "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization.As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization.As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization." ,
+      skills: [
+        {
+          name: "AWS",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: "MySQL",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: "REACT",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: "JAVA",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+      ],
+      images: [
+        {
+          img: "https://placeholder.pics/svg/300x400/16FF68-2548FF/FF608C/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+        {
+          img: "https://placeholder.pics/svg/300x400/16FF68-2548FF/FF608C/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+        {
+          img: "https://placeholder.pics/svg/300x400/16FF68-2548FF/FF608C/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+        {
+          img: "https://placeholder.pics/svg/400x400/DEDEDE/555555/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+      ],
+    },
+    {
+      projectName: "Project 2",
+      projectDescription:
+        "As an early adopter of containers and kubernetes on AWS, the company's site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization.",
+      skills: [
+        {
+          name: "AZURE",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: "SQL",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: "Angular",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+        {
+          name: ".Net",
+          description:
+            "(to manage resources in cloud computing and cloud storage as well as security credentials)",
+        },
+      ],
+      images: [
+        {
+          img: "https://placeholder.pics/svg/300x400/16FF68-2548FF/FF608C/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+        {
+          img: "https://placeholder.pics/svg/400x400/DEDEDE/555555/Project",
+          title: "Project image title",
+          featureHeading: "Feature Heading",
+          featureDescription: "As an early adopter of containers and Kubernetes on AWS, the companys site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization. As an early adopter of containers and Kubernetes on AWS, the company’s site reliability engineering (SRE) team was still using first-generation tooling that required high-effort customization."
+        },
+      ],
+    },
+  ];
